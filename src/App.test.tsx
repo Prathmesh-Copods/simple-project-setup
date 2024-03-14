81fa3fc6c70cbc0ffae3d3345dd9568b2d2d15e7
Prathmesh Dhatrak <161831954+Prathmesh-Copods@users.noreply.github.com>
2024-03-14 11:46:50 +0530

    build: configure-git-hooks-with-husky

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import user from "@testing-library/user-event";
+import { describe, it, expect } from "vitest";
 import App from "./App";
 
 describe("App", () => {
